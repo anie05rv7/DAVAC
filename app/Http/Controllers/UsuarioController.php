e58d5b3ebac31ec061e285a7d550bc8c984365ba
anie05rv7 <anie05rv7@gmail.com>
2023-11-30 17:57:14 -0600

    se instalo jwt pero aun  no se termina de configurar

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,8 +3,73 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User; 
 
-class UsuarioController extends Controller
+class UserController extends Controller
 {
-    //
+    // Mostrar todos los usuarios
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    // Crear un nuevo usuario
+    public function store(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->name;
+        $user->lastname = $request->lastname; 
+        $user->email = $request->email;
+        $user->department = $request->department; 
+        $user->phone = $request->phone;
+        $user->status = $request->status;
+        $user->token = $request->token; 
+        $user->password = bcrypt($request->password); // encriptar  contraseña
+
+        $user->save();
+
+        return response()->json(['message' => 'Usuario creado con éxito', 'user' => $user]);
+    }
+
+    // Mostrar un usuario específico
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        return response()->json($user);
+    }
+
+    // Actualizar un usuario
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        // Actualizar los campos del usuario
+        $user->update($request->all());
+
+        return response()->json(['message' => 'Usuario actualizado con éxito', 'user' => $user]);
+    }
+
+    // Eliminar un usuario
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        $user->delete();
+
+        return response()->json(['message' => 'Usuario eliminado con éxito']);
+    }
 }
