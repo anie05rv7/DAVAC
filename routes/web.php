a6ab78918983cda2fc70525a60b91f777f6ece95
CarlosRGB2 <megustaelfifa69@gmail.com>
2023-12-01 01:22:52 -0600

    Controlador Sensor y su ruta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\SensorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('/sensores', [SensorController::class, 'create']);
+Route::put('/sensores/{id}', [SensorController::class, 'update']);
