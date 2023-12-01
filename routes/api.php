864c8f78d7e707356526ce88f11928ea09af1323
CarlosRGB2 <megustaelfifa69@gmail.com>
2023-12-01 16:03:16 -0600

    Controladores de Empresa, Vitrina y sus Rutas

--- routes/api.php
+++ routes/api.php
@@ -39,6 +39,23 @@ Route::group(['middleware' => 'jwt.auth'], function () {
         Route::put('/deactivate/{id}', [UserController::class, 'deactivate']);
     });
 
+    //Rutas Sensor
+    Route::post('/sensores', [SensorController::class, 'create']);
+    Route::put('/sensores/{id}', [SensorController::class, 'update']);
+    //Rutas Empresa
+    Route::get('/empresas', [EmpresaController::class, 'index']);
+    Route::get('/empresas/{id}', [EmpresaController::class, 'show']);
+    Route::post('/empresas', [EmpresaController::class, 'create']);
+    Route::put('/empresas/{id}', [EmpresaController::class, 'update']);
+    Route::delete('/empresas/{id}', [EmpresaController::class, 'destroy']);
+    //Rutas Vitrinas
+    Route::get('/vitrinas', [VitrinaController::class, 'index']);
+    Route::get('/vitrinas/{id}', [VitrinaController::class, 'show']);
+    Route::post('/vitrinas', [VitrinaController::class, 'create']);
+    Route::put('/vitrinas/{id}', [VitrinaController::class, 'update']);
+    Route::delete('/vitrinas/{id}', [VitrinaController::class, 'destroy']);
+
+
     
 });
 
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//Rutas Sensor
 Route::post('/sensores', [SensorController::class, 'create']);
 Route::put('/sensores/{id}', [SensorController::class, 'update']);
+//Rutas Empresa
+Route::get('/empresas', [EmpresaController::class, 'index']);
+Route::get('/empresas/{id}', [EmpresaController::class, 'show']);
+Route::post('/empresas', [EmpresaController::class, 'create']);
+Route::put('/empresas/{id}', [EmpresaController::class, 'update']);
+Route::delete('/empresas/{id}', [EmpresaController::class, 'destroy']);
+ //Rutas Vitrinas
+ Route::get('/vitrinas', [VitrinaController::class, 'index']);
+ Route::get('/vitrinas/{id}', [VitrinaController::class, 'show']);
+ Route::post('/vitrinas', [VitrinaController::class, 'create']);
+ Route::put('/vitrinas/{id}', [VitrinaController::class, 'update']);
+ Route::delete('/vitrinas/{id}', [VitrinaController::class, 'destroy']);
+
+
